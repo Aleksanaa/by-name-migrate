bce1cf0dd327af2b4c048678b32c7591bfd68606
aleksana <me@aleksana.moe>
2024-11-09 19:52:31 +0800

    add nixpkgs-vet to shell

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Add a git worktree (or checkout) in ./nixpkgs:
 git worktree add /path/to/by-name-migrate/nixpkgs
 ```
 
+The script will create `/tmp/by-name-migrate`, and copy some files (about 10000 nix files) inside to test evaluation. Edit `temp_path` in script if you don't want this.
+
 Run script:
 
 ```
 nix develop
 python migrate.py
 ```
+
+It takes about 4 minutes. No multi-threading optimization here.
 
 Stage and commit all files:
 
@@ -21,11 +25,14 @@ git stage .
 git commit -m "test"
 ```
 
-Run nixpkgs-review:
+Review (in nixpkgs root):
 
 ```
 nixpkgs-review rev HEAD
+nixpkgs-vet --base . .
 ```
+
+Also you can run ofborg eval checks, see [Running meta checks locally](https://github.com/NixOS/ofborg/tree/released?tab=readme-ov-file#running-meta-checks-locally).
 
 If anything goes wrong, remove:
 
--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs-vet = {
+      url = "github:NixOS/nixpkgs-vet/0.1.4";
+      flake = false;
+    };
   };
 
   outputs =
@@ -12,7 +16,7 @@
         "aarch64-linux"
       ];
       perSystem =
-        { pkgs, ... }:
+        { pkgs, system, ... }:
         {
           devShells = {
             default = pkgs.mkShell {
@@ -23,6 +27,14 @@
                 ]))
                 pkgs.black
                 pkgs.nixpkgs-review
+                (
+                  (import inputs.nixpkgs-vet {
+                    inherit system;
+                    inherit (inputs) nixpkgs;
+                  }).build.overrideAttrs
+                  # cannot pass check?
+                  { doCheck = false; }
+                )
               ];
               shellHook = ''
                 export NIX_TREE_SITTER=${pkgs.tree-sitter-grammars.tree-sitter-nix}/parser
